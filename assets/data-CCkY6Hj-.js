f8c165ecb014d57a31c3fcfe914a59777ad4b931
Simon <runzeng13@gmail.com>
2025-04-06 14:03:51 +0800

    Add files via upload

---
+++ assets/data-CCkY6Hj-.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{r as a,c as o,o as c,a as s}from"./index-BZ1Aii_p.js";const n={},r={class:"data-page"};function _(d,i){const e=a("el-image");return c(),o("div",r,[s(e,{style:{width:"100vw",height:"100vh"},src:"/img/data.webp",fit:"contain"})])}const l=t(n,[["render",_],["__scopeId","data-v-931c1c6d"]]);export{l as default};
